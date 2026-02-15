ca88be8f0a34f897b7a3d4c89d6e15a4d3f84e84
jayekamau-1991 <jayekamau@gmail.com>
2026-02-15 09:27:19 -0500

    Consolidate Lab1a: move Lab1_deliverables contents into Lab1a folder

--- lab-1/1a/gate_network_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script is designed to manage gate networks in the database.
-
-# Add your commands here
-
-echo "Gate Network Database Management Script"
--- lab-1/1b/1b_lab.md
+++
@@ -1,5 +0,0 @@
-# Lab 1B
-
-This is the 1B lab documentation.
-
-Date Created: 2026-02-14 21:28:25 (UTC)
