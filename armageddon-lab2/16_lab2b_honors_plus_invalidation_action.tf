4ecd7089a0fc19e799a8cb010121cf79a16d18ef
jayekamau-1991 <jayekamau@gmail.com>
2026-02-15 08:48:02 -0500

    Delete armageddon-lab2 directory

--- armageddon-lab2/16_lab2b_honors_plus_invalidation_action.tf
+++
@@ -1,8 +0,0 @@
-# resource "aws_cloudfront_create_invalidation" "armageddon_invalidate_index01" {
-#   distribution_id = aws_cloudfront_distribution.armageddon_cf01.id
-
-
-#   paths = [
-#     "/static/index.html"
-#   ]
-# }
